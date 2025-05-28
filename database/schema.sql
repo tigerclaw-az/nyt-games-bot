0a329c6db7be5ba6bffd3c7eb187a86f2938a53c
Erik Waibel <ejwaibel@gmail.com>
2025-05-28 08:56:00 -0700

    Update SQL statements to use INTEGER

--- database/schema.sql
+++ database/schema.sql
@@ -1,37 +1,37 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` VARCHAR(50) PRIMARY KEY,
+  `user_id` INTEGER NOT NULL PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `connections` (
-  `puzzle_id` INT,
-  `user_id` VARCHAR(50),
+  `puzzle_id` INTEGER,
+  `user_id` INTEGER,
   `puzzle_str` TEXT NOT NULL,
-  `score` INT NOT NULL,
+  `score` INTEGER NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`puzzle_id`, `user_id`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `strands` (
-  `puzzle_id` INT,
-  `user_id` VARCHAR(50),
+  `puzzle_id` INTEGER,
+  `user_id` INTEGER,
   `puzzle_str` TEXT NOT NULL,
-  `hints` INT NOT NULL,
+  `hINTEGERs` INTEGER NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`puzzle_id`, `user_id`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `wordle` (
-  `puzzle_id` INT,
-  `user_id` VARCHAR(50),
+  `puzzle_id` INTEGER,
+  `user_id` INTEGER,
   `puzzle_str` TEXT NOT NULL,
-  `score` INT NOT NULL,
-  `green` INT NOT NULL,
-  `yellow` INT NOT NULL,
-  `other` INT NOT NULL,
+  `score` INTEGER NOT NULL,
+  `green` INTEGER NOT NULL,
+  `yellow` INTEGER NOT NULL,
+  `other` INTEGER NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`puzzle_id`, `user_id`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
